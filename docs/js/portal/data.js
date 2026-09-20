35c73a1d26d9d37d1cacb053482c3c51e88a8f8e
Pasquelin Alban <alban.pasquelin@gmail.com>
2026-09-21 00:26:44 +0200

    docs(sdk): memory pools and cut admission guide, budget threshold in the bench (#24)

--- docs/js/portal/data.js
+++ docs/js/portal/data.js
@@ -1,4 +1,5 @@
 import { EXAMPLES, GUIDES } from '../docsContentGuides.js';
+import { ENGINE_GUIDES } from '../docsContentGuidesEngine.js';
 import { ENUMS_IMAGE } from '../docsContentEnums.js';
 import { ENUMS_RUNTIME } from '../docsContentEnumsRuntime.js';
 import { LIFECYCLE } from '../docsContentLifecycle.js';
@@ -10,6 +11,7 @@ import { BATCHES, TREE } from '../docsContentTree.js';
 
 export const rawEntries = [
   ...GUIDES,
+  ...ENGINE_GUIDES,
   ...EXAMPLES,
   ...ENUMS_IMAGE,
   ...ENUMS_RUNTIME,
